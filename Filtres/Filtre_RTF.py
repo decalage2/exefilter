bd027cfbb9d5f91ff7182e80b8aa19db63e91123
Philippe Lagadec <decalage2@users.noreply.github.com>
2010-02-23 22:12:02 +0000

    updated RechercherRemplacer import in several filters

--- Filtres/Filtre_RTF.py
+++ Filtres/Filtre_RTF.py
@@ -17,26 +17,26 @@ URL du projet: U{http://admisource.gouv.fr/projects/exefilter}
 @contact: U{Philippe Lagadec<mailto:philippe.lagadec(a)laposte.net>}
 
 @copyright: DGA/CELAR 2004-2008
-@copyright: NATO/NC3A 2008-2009 (modifications PL apres v1.1.0)
+@copyright: NATO/NC3A 2008-2010 (modifications PL apres ExeFilter v1.1.0)
 
 @license: CeCILL (open-source compatible GPL)
           cf. code source ou fichier LICENCE.txt joint
 
-@version: 1.03
+@version: 1.04
 
 @status: beta
 """
 #==============================================================================
 __docformat__ = 'epytext en'
 
-__date__    = "2009-11-02"
-__version__ = "1.03"
+__date__    = "2010-02-23"
+__version__ = "1.04"
 
 #------------------------------------------------------------------------------
 # LICENCE pour le projet ExeFilter:
 
 # Copyright DGA/CELAR 2004-2008
-# Copyright NATO/NC3A 2008-2009 (PL changes after v1.1.0)
+# Copyright NATO/NC3A 2008-2010 (PL changes after ExeFilter v1.1.0)
 # Auteurs:
 # - Philippe Lagadec (PL) - philippe.lagadec(a)laposte.net
 # - Arnaud Kerréneur (AK) - arnaud.kerreneur(a)dga.defense.gouv.fr
@@ -80,6 +80,7 @@ __version__ = "1.03"
 # 2008-03-24 v1.02 PL: - ajout de _() pour traduction gettext des chaines
 #                      - simplification dans nettoyer() en appelant resultat_*
 # 2009-11-02 v1.03 PL: - updated parameters for gettext translation
+# 2010-02-23 v1.04 PL: - updated RechercherRemplacer import
 
 #------------------------------------------------------------------------------
 # A FAIRE:
@@ -91,7 +92,7 @@ __version__ = "1.03"
 import os, os.path, re, binascii, tempfile
 
 # modules spécifiques:
-import RechercherRemplacer
+import thirdparty.RechercherRemplacer.RechercherRemplacer as RechercherRemplacer
 
 # modules du projet:
 from commun import *
